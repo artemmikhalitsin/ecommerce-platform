c3c0b8f5451c58ed911932ea4d8565227213fe79
artemmikhalitsin <artemmikhalitsin@gmail.com>
2017-09-22 16:32:17 -0400

    Added a script testing the mysql node driver functionality

--- app/app/database/connection.js
+++ app/app/database/connection.js
@@ -1,9 +1,35 @@
-const mysql = require('mysql');
 const mysqlOptions = {
-     host: 'localhost',
-     user: 'me',
-     password: 'secret',
-     database: 'my_db'
+     client: 'mysql',
+     connection:{
+          host: 'localhost',
+          user: 'root',
+          password: '343docker',
+          database: '343DB'
+     }
 }
+const mysql = require('knex')(mysqlOptions)
 
-var connection = mysql.createConnection
+mysql.schema.createTableIfNotExists('customers', (table)=>{
+     if(!table){
+          console.log("Error creating table")
+     }
+     else{
+          table.increments()
+          table.string('name')
+          table.string('address')
+     }
+}).then( () => {
+     customer = {
+          name: 'Davey Jones',
+          address: 'His Locker'
+     }
+
+     console.log("Inserting Davey Jones")
+     mysql('customers').insert(customer).then((res) =>{
+          console.log(res)
+     })
+
+     mysql.select('*').from('customers').then((rows) => {
+          console.log(rows)
+     })
+})
