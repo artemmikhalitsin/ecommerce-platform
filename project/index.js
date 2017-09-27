8c448529002a53711df2dd8ef271bba2a7e2d650
Wai <wl.wailau@gmail.com>
2017-09-27 14:53:28 -0400

    added routes and .html files for front end dev

--- project/index.js
+++ project/index.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.sendFile('public/testpage.html' , { root : __dirname});
 })
 
+app.get('/login', function (req, res) {
+  res.sendFile('public/login.html' , { root : __dirname});
+})
+
+app.get('/database', function (req, res) {
+  res.sendFile('public/database.html' , { root : __dirname});
+})
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 })
