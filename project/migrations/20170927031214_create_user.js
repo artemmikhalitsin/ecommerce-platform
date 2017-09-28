f7db4d3f931052905e81a77557c5a03f425636e2
Amanda <amawai@github.com>
2017-09-28 18:09:58 -0400

    ARTEM: Migrations and seeding scripts for database

--- project/migrations/20170927031214_create_user.js
+++ project/migrations/20170927031214_create_user.js
@@ -6,11 +6,13 @@ exports.up = function(knex, Promise) {
       throw new Error('Error creating table ' + tablename)
     }
     else {
-      table.string('email').primary()
+      table.increments('id').unsigned().primary()
+      table.string('email').notNullable()
       table.string('first_name').notNullable()
       table.string('last_name').notNullable()
       table.string('password').notNullable()
       table.string('full_address').notNullable()
+      table.boolean('is_admin').notNullable()
       table.integer('phone_number').unsigned().notNullable()
     }
   })
