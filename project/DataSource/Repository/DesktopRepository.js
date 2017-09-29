ca3380ca76c875d370b88570bbb79e09133a2387
katiaru <katiaruhlin@gmail.com>
2017-09-29 16:41:10 -0400

    add desktop implementation

--- project/DataSource/Repository/DesktopRepository.js
+++ project/DataSource/Repository/DesktopRepository.js
@@ -1,21 +1,13 @@
 'use strict';
-const express = require('express');
-const app = express();
 
 const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
+const rootPath = require('app-root-dir').get()
+const configuration = require(rootPath + '/knexfile')[environment];
 const database = require('knex')(configuration);
 
 function save(desktop){
-  database('Desktop').insert(desktop)
-    .then(desktop => {
-      res.status(200).json(desktop)
-      return res.send(desktop);
-    })
-    .catch(error => {
-      res.status(500).json({error});
-      return res.send(desktop);
-    });
+  console.log(desktop)
+  return database('Desktop').insert(desktop);
 };
 
 module.exports = {
