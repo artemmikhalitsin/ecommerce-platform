070ed8adce3f8e87ccfb2ee5abd489263bc4f903
artemmikhalitsin <artemmikhalitsin@gmail.com>
2017-09-29 01:11:37 -0400

    Introduced a rendering engine.

--- project/index.js
+++ project/index.js
@@ -1,28 +1,40 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+
 const app = express();
 
+app.engine('hbs', hbs({extname: 'hbs'}));
+app.set('views', path.join(__dirname, 'public'));
+app.set('view engine', 'hbs');
+
 app.get('/', function(req, res) {
-  res.sendFile('public/testpage.html', {root: __dirname});
+  res.render('testpage');
 });
 
+app.get('/test', function(req, res) {
+  res.render('hbs_test', {body: "Nice bod, bro"})
+})
+
 app.get('/login', function(req, res) {
-  res.sendFile('public/login.html', {root: __dirname});
+  res.render('login');
 });
 
 app.get('/registration', function(req, res) {
-  res.sendFile('public/registration.html', {root: __dirname});
+  res.render('registration');
 });
 
 app.get('/inventory', function(req, res) {
-  res.sendFile('public/inventory.html', {root: __dirname});
+  res.render('inventory');
 });
 
 app.get('/database', function(req, res) {
-  res.sendFile('public/database.html', {root: __dirname});
+  res.render('database');
 });
 
 app.get('/admin', function(req, res) {
-  res.sendFile('public/admin.html', {root: __dirname});
+  res.render('admin');
 });
 
 app.listen(8080, function() {
