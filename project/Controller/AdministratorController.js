2ebe243f336eb029987be18eb86c83dd5b4f4114
artemmikhalitsin <artemmikhalitsin@gmail.com>
2017-09-29 11:51:43 -0400

    Preparing for merge

---
+++ project/Controller/AdministratorController.js
@@ -0,0 +1,9 @@
+var AdministratorController = function(){
+
+    var deskTopRepo = new DesktopRepository();
+
+};
+
+AdministratorController.prototype.save = function(desktop){
+    deskTopRepo.save(desktop);
+}
---
+++ project/DataSource/Repository/PurchaseRepository.js
@@ -0,0 +1,23 @@
+'use strict';
+var PurcheRepository = function(){
+  const express = require('express');
+  const app = express();
+
+  const environment = process.env.NODE_ENV || 'development';
+  const configuration = require('./knexfile')[environment];
+  const database = require('knex')(configuration);
+
+  
+};
+PurcheRepository.prototype.save = function(purchase){
+  
+      database('Purchase').insert(purchase)
+        .then(purchase => {
+          res.status(200).json(purchase)
+          return res.send(purchase);
+        })
+        .catch(error => {
+          res.status(500).json({error});
+          return res.send(purchase);
+        });
+    };
