939e8d960d20ff82821b15fc285a8c4532ad60e1
Wai <wl.wailau@gmail.com>
2017-09-29 11:12:18 -0400

    fixed style

--- project/public/js/addItem.js
+++ project/public/js/addItem.js
@@ -3,21 +3,21 @@ let postDesktop = (desktopData) => {
         url: 'http://localhost:8080/Controller/AdministratorController/save',
         type: 'POST',
         data: desktopData,
-        success: function(data){
-            alert("success!");
+        success: function(data) {
+            alert('success!');
             console.log(data);
         },
-        error: function(error){
+        error: function(error) {
             console.log(error);
-        }
-    })
-}
+        },
+    });
+};
 let addDesktop = () => {
-    alert("in add method");
+    alert('in add method');
     let desktopData = $('form').serializeArray().reduce((obj, desktop) => {
         obj[desktop.name] = desktop.value;
         return obj;
     }, {});
-    alert("in add desktop");
+    alert('in add desktop');
         postDesktop(desktopData);
-}
+};
