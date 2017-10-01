d5aa95f5e9e4b011af6cfb1fe8edb88bab90b207
Wai <wl.wailau@gmail.com>
2017-10-01 19:26:56 -0400

    removed jsdoc requirement from linter

--- project/Controller/AdministratorController.js
+++
@@ -1,8 +0,0 @@
-let AdministratorController = function() {
-    let deskTopRepo = new DesktopRepository();
-};
-
-AdministratorController.prototype.save = function(desktop) {
-    console.log('Im alive');
-    deskTopRepo.save(desktop);
-};
