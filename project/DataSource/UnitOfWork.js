d075ac09bebc322cebbe4dd4dd6695bd472534e5
katiaru <katiaruhlin@gmail.com>
2017-10-12 09:45:46 -0400

    working uow

--- project/DataSource/UnitOfWork.js
+++ project/DataSource/UnitOfWork.js
@@ -10,23 +10,12 @@ var Promise = require('bluebird');
     }
     
    function commit(object, tableName){
-       connection.transaction(function(trx){
+       return connection.transaction(function(trx){
             connection.insert(object)
-                    .insert(tableName)
+                    .into(tableName)
                     .transacting(trx)
-                    /*.then(function(){
-                        //do something here
-                    }) */
                     .then(trx.commit)
                     .catch(trx.rollback);
-       })
-       .then(function(res){
-           //worked
-           console.log("in then");
-           //return res;
-       })
-       .catch(function(e){
-           //failed
        });
    }
    module.exports = {
