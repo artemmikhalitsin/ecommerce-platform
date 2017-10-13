54c00cf894df0546cf280031018f1ced6fb023eb
artemmikhalitsin <artemmikhalitsin@gmail.com>
2017-10-13 16:43:45 -0400

    Interactive product management form

--- project/public/inventory/new-product.module.js
+++ project/public/inventory/new-product.module.js
@@ -1,5 +1,6 @@
-var newProduct = angular.module('newProduct', ['tvForm'])
+var newProduct = angular.module('newProduct',
+    ['tvForm', 'monitorForm', 'laptopForm', 'desktopForm', 'tabletForm'])
 
 newProduct.controller('FormController', ['$scope', function($scope) {
-  $scope.selected = 'laptop'
+  $scope.selected = ''
 }]);
--- project/public/inventory/product-forms/tv-form.component.js
+++
@@ -1,6 +0,0 @@
-angular
-  .module('tvForm')
-  .component('tvForm', {
-    templateUrl: '/inventory/product-forms/tv-form.html',
-    controller: function() {},
-      });
