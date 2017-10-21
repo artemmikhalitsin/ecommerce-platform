08f3ad786ef09a16ab786baed2eb7d4791c022f4
artemmikhalitsin <artemmikhalitsin@gmail.com>
2017-10-20 22:13:29 -0400

    Working on integrating angular

--- project/public/inventory/new-product.module.js
+++ project/public/inventory/new-product.module.js
@@ -3,4 +3,11 @@ var newProduct = angular.module('newProduct',
 
 newProduct.controller('FormController', ['$scope', function($scope) {
   $scope.selected = ''
+  $scope.$on('newLaptop', addLaptopToList)
+
+
+  function addLaptopToList(event, args) {
+    console.log('addlaptop')
+    //do something with the laptop(args)
+  }
 }]);
--- project/public/inventory/product-forms/laptop-form.module.js
+++ project/public/inventory/product-forms/laptop-form.module.js
@@ -3,5 +3,11 @@ var laptopForm = angular.module('laptopForm', []);
 //Component data
 laptopForm.component('laptopForm', {
   templateUrl: '/inventory/product-forms/laptop-form.html',
-  controller: function() {},
-    });
+  controller: function($scope) {
+    $scope.laptop = {}
+
+    $scope.submitLaptop = () => {
+      $scope.$emit('newLaptop', $scope.laptop)
+    }
+  },
+});
