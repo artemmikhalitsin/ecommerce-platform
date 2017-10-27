02402f46c06da9257b7f0ca1d80515ac1e18f2d9
Christina Nguyen <christina.thao.nguyen@hotmail.com>
2017-10-27 12:10:45 -0400

    Delete files to pull new ones

--- project/public/inventory/new-product.module.js
+++
@@ -1,6 +0,0 @@
-var newProduct = angular.module('newProduct',
-    ['tvForm', 'monitorForm', 'laptopForm', 'desktopForm', 'tabletForm'])
-
-newProduct.controller('FormController', ['$scope', function($scope) {
-  $scope.selected = ''
-}]);
