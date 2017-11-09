4c4d370d9a8cb83927b03b468c04a656400b8c7b
Michael <li.michael.91@gmail.com>
2017-11-09 17:28:25 -0500

    Add PurchseCollection Table and assciatted files. Such as the repo and TDG.

---
+++ project/DataSource/TableDataGateway/purchaseCollectionTDG.js
@@ -0,0 +1,53 @@
+const environment = process.env.NODE_ENV || 'development';
+const rootPath = require('app-root-dir').get();
+const configuration = require(rootPath + '/knexfile')[environment];
+const connection = require('knex')(configuration);
+
+/**
+ * Table Data Gateway of inventory items table
+ * @author Michael Li
+ * REVIEW: PLEASE MAKE SURE THE METHOD DESCRIPTIONS ARE correct
+ * REVIEW: PLEASE VERIFY TYPES OF PROMISES - Artem
+ */
+class InventoryItemsTDG {
+    /**
+     * Adds a new item into the table
+     * @param {string} clientID The unique id of the client.
+     * @param {string} serialNumber The unique serial number of the item
+     * @param {string} modelNumber The model number to associate the serial to
+     * @return {Promise<number[]>} a promise which resolves to an array
+     * containing the id of the inserted item in the table
+     */
+    add(serialNumber, modelNumber) {
+        return connection.insert({
+            'client_id': clientID,
+            'serial_number': serialNumber,
+            'model_number': modelNumber,
+          }, 'id')
+        .into('PurchaseCollection');
+    }
+
+    /**
+     * Gets all items from the table
+     * @return {Promise<Object[]>} a promise which resolves to an array of items
+     */
+    select() {
+        return connection('PurchaseCollection').select('*');
+    }
+    // update is not in current requirements for inventory items
+    /* update(inventoryItems){
+    }*/
+
+    /**
+     * Deletes an item from the inventory given an id
+     * @param {number} inventoryItem the id of the item to be deleted,
+     * as it appears in the table
+     * @return {Promise<number>} a promise which resolves to the number of rows
+     * affected
+     */
+    delete(inventoryItem) {
+        // TODO
+        return connection.from('PurchaseCollection').where({id: inventoryItem.id}).del();
+    }
+}
+module.exports = InventoryItemsTDG;
