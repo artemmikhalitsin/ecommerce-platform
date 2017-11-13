51e6add71f20166649445dafd1022649682fd6e5
Christina Nguyen <christina.thao.nguyen@hotmail.com>
2017-11-13 10:34:32 -0500

    to revert

---
+++ project/public/inventory/product-forms/tv-form.module.js
@@ -0,0 +1,8 @@
+var tvForm = angular.module('tvForm', []);
+
+//Component data
+tvForm.component('tvForm', {
+  templateUrl: '/inventory/product-forms/tv-form.html',
+  controller: function() {
+  },
+});
