d911ede6f288051fa623731db964608192efb4f3
Christina Nguyen <christina.thao.nguyen@hotmail.com>
2017-11-16 13:47:48 -0500

    Added files
    Usesr TDG and Identity map

---
+++ project/DataSource/TableDataGateway/UsersTDG.js
@@ -0,0 +1,36 @@
+const environment = process.env.NODE_ENV || 'development';
+const rootPath = require('app-root-dir').get();
+const configuration = require(rootPath + '/knexfile')[environment];
+const connection = require('knex')(configuration);
+
+class UsersTDG{
+  /**
+   * Adds a new user into the table
+   * @param {integer} id The unique id of the user
+   * @return {Promise<number[]>} a promise which resolves to an array
+   * containing the id of the inserted user in the table
+   */
+  add(user){
+    return connnection.inser({
+      'first_name': user.first_name,
+      'last_name': user.last_name,
+      'address': user.address,
+      'email': user.email,
+      'phone_number': user.phone_number,
+    }, 'id').into('Users');
+  }
+  /**
+   * Deletes an item from the inventory given an id
+   * @param {id} user the id of the user to be deleted,
+   * as it appears in the table
+   * @return {Promise<number>} a promise which resolves to the id of user
+   * affected
+   */
+  delete(user){
+    return connection.from('Users'). where(
+      {id: user.id}
+    ).del();
+  }
+}
+
+module.exports = UsersTDG;
