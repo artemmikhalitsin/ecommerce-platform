e97fede8bb326a4320e1f1dc50d96e3b3bcfe495
Ricardo Liganor <ricardo.liganor@hotmail.com>
2017-11-16 00:27:26 -0500

    start to work on user table

---
+++ project/test/producRepo.js
@@ -0,0 +1,8 @@
+const rootPath = require('app-root-dir').get();
+const userRepo = require(rootPath + '/DataSource/Repository/UserRepository.js');
+
+let repo = new userRepo();
+
+repo.getAdmins().then(
+  (result) => { console.log(`Response from server: ${JSON.stringify(result)}`)}
+)
