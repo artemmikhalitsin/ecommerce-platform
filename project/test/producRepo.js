75d3fa2cf8b9444ab61968abd073881d38423983
Ricardo Liganor <ricardo.liganor@hotmail.com>
2017-11-16 12:19:25 -0500

    product cards update

--- project/test/producRepo.js
+++ project/test/producRepo.js
@@ -1,8 +1,8 @@
 const rootPath = require('app-root-dir').get();
-const userRepo = require(rootPath + '/DataSource/Repository/UserRepository.js');
+const userRepo = require(rootPath + '/DataSource/Repository/InventoryItemRepository.js');
 
 let repo = new userRepo();
 
-repo.getAdmins().then(
+repo.getAllInventoryItems().then(
   (result) => { console.log(`Response from server: ${JSON.stringify(result)}`)}
 )
