fa8e6a722f3866d2edef548f613b0330bf93d3b8
Drapha <danielisakov@yahoo.ca>
2017-11-24 15:50:23 -0500

    reverted pre-artem merge
    This reverts commit 99ced4823df4c4ff1c7f08cb00ca54a52d839ca8.

--- project/test/IMtest.js
+++ project/test/IMtest.js
@@ -3,6 +3,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 chai.use(require('chai-http'));
+// const sinon = require('sinon');
 
 const rootPath = require('app-root-dir').get();
 const InvIdentityMap = require(rootPath + '/DataSource/IdentityMap/'
@@ -15,13 +16,14 @@ const InventoryItem = require(rootPath + '/models/InventoryItem.js');
 const ProductDescription = require(rootPath + '/models/ProductDescription.js');
 const User = require(rootPath + '/models/User.js');
 
-describe('Inventory Item Identity Map Unit Testing', function() {
+describe('Inventory Item Unit Testing', function() {
     ()=>timeout(5000);
     let iiim = new InvIdentityMap();
     const im1 = new InventoryItem(0, 'app', '0H', null);
     const im2 = new InventoryItem(1, 'app', '5H', null);
     const im3 = new InventoryItem(2, 'app', '1C', null);
     const im4 = new InventoryItem(3, 'ppa', '5Q', null);
+    const list = [im1, im2, im3, im4];
     beforeEach(function() {
     });
 
@@ -29,41 +31,33 @@ describe('Inventory Item Identity Map Unit Testing', function() {
     });
 
     it('add new inventory items to identity map and'
-    +' be able to retrieve them', function() {
-        iiim.add(im4);
-        iiim.add(im1);
-        iiim.add(im3);
-        iiim.add(im2);
-        expect(iiim.getAll()).to.include(im4);
-        expect(iiim.getAll()).to.include(im2);
-        expect(iiim.getAll()).to.include(im1);
-        expect(iiim.getAll()).to.include(im3);
+    +'be able to retrieve them', function() {
+        iiim.add(list);
+        expect(iiim.getAll()).to.have.deep.members(list);
     });
 
-    it('be able to retrieve specific items by their serial'
-    + 'or model number', function() {
-        const serial = 'ppa';
-        const model = '5H';
-        expect(iiim.get(serial)).to.have.property('serialNumber', serial);
-        (iiim.getByModelNumber(model).every((item) => expect(item)
-            .to.have.property('modelNumber', model)));
-            });
+    it('be able to retrieve a specific item by its model number', function() {
+        const modNumList = 'ppa';
+        (iiim.get(modNumList)).every((item) => expect(item).to.have
+        .property('serialNumber', 'ppa'));
+    });
 
     it('be able to delete specific items by their model number', function() {
-        const serial = '0H';
-        iiim.delete(serial);
-        (iiim.getAll()).every((item) => expect(item).to
-            .not.have.property('serialNumber', serial));
+        const modNumList = 'app';
+        iiim.delete(modNumList);
+        (iiim.getAll()).every((item) => expect(item).to.not.have
+        .property('serialNumber', 'app'));
     });
 });
 
-describe('Product Description Identity Map Unit Testing', function() {
+describe('Prouct Description Unit Testing', function() {
     ()=>timeout(5000);
     let piim = new ProdIdentityMap();
     const pd1 = new ProductDescription('1$', '2g', 'app', '1H', true, 'a');
     const pd2 = new ProductDescription('1$', '2g', 'paa', '2H', true, 'a');
     const pd3 = new ProductDescription('1$', '2g', 'app', '0H', true, 'a');
     const pd4 = new ProductDescription('1$', '2g', 'app', '3H', true, 'a');
+    const list = [pd1, pd2, pd3, pd4];
     beforeEach(function() {
     });
 
@@ -71,41 +65,35 @@ describe('Product Description Identity Map Unit Testing', function() {
     });
 
     it('add new product descriptions to identity map and'
-    +' be able to retrieve them', function() {
-        piim.add(pd1);
-        piim.add(pd2);
-        piim.add(pd3);
-        piim.add(pd4);
-        expect(piim.getAll()).to.include(pd1);
-        expect(piim.getAll()).to.include(pd2);
-        expect(piim.getAll()).to.include(pd3);
-        expect(piim.getAll()).to.include(pd4);
+    +'be able to retrieve them', function() {
+        piim.add(list);
+        expect(piim.getAll()).to.have.deep.members(list);
     });
 
-    it('be able to retrieve specific descriptions by'
-    +' their model number', function() {
-        const modNumList = '0H';
-        expect(piim.get(modNumList)).to.have.property('modelNumber',
-        modNumList);
+    it('be able to retrieve a specific description by'
+    +'its model number', function() {
+        const modNumList = 'app';
+        (piim.get(modNumList)).every((desc) => expect(desc).to.have
+        .property('serialNumber', 'app'));
     });
 
-    // it('be able to update a description'
-    // +' by their model number', function() {
-    //     const oldDescs = piim.getAll();
-    //     const modNumToChange = ['app'];
-    //     // every updatedItem is assumed to have a unique model number
-    //     let updatedItems = [];
-    //     for (let i=0; i<modNumToChange.length; i++) {
-    //         updatedItems.push(new ProductDescription('4$', '5g',
-    //             modNumToChange[i], '0H', 'a'));
-    //     }
-    //     piim.update(updatedItems);
-    //     const upDescs = (piim.getAll()).filter((d) => {
-    //         return modNumToChange.includes(d.modelNumber);
-    //     });
-    //     expect(upDescs).to.not.include(upDescs);
-    //     expect(piim.getAll()).to.have.property('length', oldDescs.length);
-    // });
+    it('be able to update a description'
+    +'by their model number', function() {
+        const oldDescs = piim.getAll();
+        const modNumToChange = ['app'];
+        // every updatedItem is assumed to have a unique model number
+        let updatedItems = [];
+        for (let i=0; i<modNumToChange.length; i++) {
+            updatedItems.push(new ProductDescription('4$', '5g',
+                modNumToChange[i], '0H', 'a'));
+        }
+        piim.update(updatedItems);
+        const upDescs = (piim.getAll()).filter((d) => {
+            return modNumToChange.includes(d.modelNumber);
+        });
+        expect(upDescs).to.not.include(upDescs);
+        expect(piim.getAll()).to.have.property('length', oldDescs.length);
+    });
 
     it('be able to delete specific descriptions'
     +'by their model number', function() {
