99ced4823df4c4ff1c7f08cb00ca54a52d839ca8
danielisakov@hotmail.com <danielisakov@hotmail.com>
2017-11-23 23:25:32 -0500

    identity map test cases done

--- project/test/IMtest.js
+++ project/test/IMtest.js
@@ -3,7 +3,6 @@
 const chai = require('chai');
 const expect = chai.expect;
 chai.use(require('chai-http'));
-const sinon = require('sinon');
 
 const rootPath = require('app-root-dir').get();
 const InvIdentityMap = require(rootPath + '/DataSource/IdentityMap/'
@@ -14,8 +13,9 @@ const UsersIdentityMap = require(rootPath + '/DataSource/IdentityMap/'
     +'UsersIdentityMap.js');
 const InventoryItem = require(rootPath + '/models/InventoryItem.js');
 const ProductDescription = require(rootPath + '/models/ProductDescription.js');
+const User = require(rootPath + '/models/User.js');
 
-describe('Inventory Item Unit Testing', function() {
+describe('Inventory Item Identity Map Unit Testing', function() {
     ()=>timeout(5000);
     let iiim = new InvIdentityMap();
     const im1 = new InventoryItem(0, 'app', '0H', null);
@@ -30,26 +30,26 @@ describe('Inventory Item Unit Testing', function() {
     });
 
     it('add new inventory items to identity map and'
-    +'be able to retrieve them', function() {
+    +' be able to retrieve them', function() {
         iiim.add(list);
         expect(iiim.getAll()).to.have.deep.members(list);
     });
 
-    it('be able to retrieve a specific item by its model number', function() {
+    it('be able to retrieve specific items by their model number', function() {
         const modNumList = 'ppa';
         (iiim.get(modNumList)).every((item) => expect(item).to.have
-        .property('serialNumber', 'ppa'));
+        .property('serialNumber', modNumList));
     });
 
     it('be able to delete specific items by their model number', function() {
         const modNumList = 'app';
         iiim.delete(modNumList);
         (iiim.getAll()).every((item) => expect(item).to.not.have
-        .property('serialNumber', 'app'));
+        .property('serialNumber', modNumList));
     });
 });
 
-describe('Prouct Description Unit Testing', function() {
+describe('Product Description Identity Map Unit Testing', function() {
     ()=>timeout(5000);
     let piim = new ProdIdentityMap();
     const pd1 = new ProductDescription('1$', '2g', 'app', '0H', 'a');
@@ -64,20 +64,20 @@ describe('Prouct Description Unit Testing', function() {
     });
 
     it('add new product descriptions to identity map and'
-    +'be able to retrieve them', function() {
+    +' be able to retrieve them', function() {
         piim.add(list);
         expect(piim.getAll()).to.have.deep.members(list);
     });
 
-    it('be able to retrieve a specific description by'
-    +'its model number', function() {
+    it('be able to retrieve specific descriptions by'
+    +' their model number', function() {
         const modNumList = 'app';
         (piim.get(modNumList)).every((desc) => expect(desc).to.have
-        .property('serialNumber', 'app'));
+        .property('serialNumber', modNumList));
     });
 
     it('be able to update a description'
-    +'by their model number', function() {
+    +' by their model number', function() {
         const oldDescs = piim.getAll();
         const modNumToChange = ['app'];
         // every updatedItem is assumed to have a unique model number
@@ -99,6 +99,50 @@ describe('Prouct Description Unit Testing', function() {
         const modNumList = 'app';
         piim.delete(modNumList);
         (piim.getAll()).every((desc) => expect(desc).to.not.have
-        .property('serialNumber', 'app'));
+        .property('serialNumber', modNumList));
+    });
+});
+
+describe('Users Identity Map Unit Testing', function() {
+    let uiim = new UsersIdentityMap();
+    const u1 = new User('a@a.a', 'Billy', '123 ms', 'Bob', false);
+    const u2 = new User('b@b.a', 'Elfie', '123 ms', 'Aoa', false);
+    const u3 = new User('c@c.a', 'Vinny', '123 ms', 'Aba', false);
+    const u4 = new User('a@a.a', 'Raspy', '123 ms', 'Bab', false);
+    const list = [u1, u2, u3, u4];
+    beforeEach(function() {
+    });
+
+    after(function() {
+    });
+
+    it('add new users to identity map and'
+    +' be able to retrieve them', function() {
+        uiim.add(list);
+        expect(uiim.getAll()).to.have.deep.members(list);
+    });
+
+    it('be able to retrieve specific users by their email', function() {
+        const dupEmail = 'a@a.a';
+        (uiim.getByEmail(dupEmail)).every((user) => expect(user).to.have
+        .property('email', dupEmail));
+    });
+
+    it('be able to retrieve a specific user by a combination of'
+    +' email and password', function() {
+        const emailCorr = 'b@b.a';
+        const passCorr = 'Aoa';
+        expect(uiim.getByEmailAndPassword(emailCorr, passCorr)).to.have
+        .property('email', emailCorr);
+        expect(uiim.getByEmailAndPassword(emailCorr, passCorr)).to.have
+        .property('password', passCorr);
+    });
+
+    it('be able to delete a user from identity map'
+    +'by their email', function() {
+        const dupEmail = 'a@a.a';
+        uiim.delete(dupEmail);
+        (uiim.getAll()).every((user) => expect(user).to.not.have
+        .property('email', dupEmail));
     });
 });
